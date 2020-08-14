7deb329eae7092f0bb8a4d3980c2ab2d2b534faa
MarllonCampos <marlloncampos@outlook.com>
2020-08-14 16:04:24 -0300

    Criado o diretorio TodoList e movido os arquivos para la

---
+++ ToDoList/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var boxElement = document.querySelector('.box');
+console.log(boxElement);
+boxElement.style.width = 100;
+boxElement.style.height = 100;
+boxElement.style.backgroundColor = "#111";
