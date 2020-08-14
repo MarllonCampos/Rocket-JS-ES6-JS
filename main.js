79d0f42d6849d5fd420cb7276769c27fc798087d
MarllonCampos <marlloncampos@outlook.com>
2020-08-14 15:03:37 -0300

    Começando a realizar os desafios de manipulaçao de DOM

---
+++ main.js
@@ -0,0 +1,7 @@
+var boxElement = document.querySelector('.box');
+
+console.log(boxElement);
+
+boxElement.style.width = 100
+boxElement.style.height = 100
+boxElement.style.backgroundColor = "#111"
