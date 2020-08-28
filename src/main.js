f0d10b3a0d4251a970f7871190a773595c393b80
MarllonCampos <marlloncampos@outlook.com>
2020-08-28 11:20:20 -0300

    Adicionado o axios, criado a page de listagem de repositórios, e seu js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,99 @@
-import axios from 'axios'
+import api from './api'
 
-class Api {
-    static async getUserInfo(username) {
+
+class App {
+    constructor() {
+        this.repositories = []
+        this.formEl = document.querySelector('#repo-form')
+        this.inputEl = document.querySelector('input[name=repository]')
+        this.listEl = document.querySelector('#repo-list')
+
+        this.registerHandlers()
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event)
+    }
+
+    setLoading(loading = true) {
+        if (loading) {
+            let loadingEl = document.createElement('span')
+            loadingEl.appendChild(document.createTextNode('Carregando...'))
+            loadingEl.setAttribute('id', 'loading')
+
+            this.formEl.appendChild(loadingEl)
+        } else {
+            document.querySelector('#loading').remove()
+        }
+    }
+    async addRepository(event) {
+        event.preventDefault()
+        const repoInput = this.inputEl.value
+
+        if (repoInput.length == 0) { return; }
+
+
+        this.setLoading();
         try {
-            const response = await axios.get(`https://api.github.com/users/${username}`)
-            console.log(response.data);
-        } catch (err) {
-            console.warn('Erro na API')
+            const response = await api.get(`/repos/${repoInput}`)
+
+            let { name, description, html_url, owner: { avatar_url } } = response.data
+
+            if (!description) {
+                description = 'Não há descrições disponíveis'
+            }
+            console.log(avatar_url);
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            })
+
+            this.inputEl.value = ""
+            this.render()
+        } catch (e) {
+            alert('O repositório não existe!')
         }
 
+        this.setLoading(false)
     }
+
+    render() {
+        this.listEl.innerHTML = ""
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img')
+            imgEl.setAttribute('src', repo.avatar_url)
+
+            let titleEl = document.createElement('strong')
+            titleEl.appendChild(document.createTextNode(repo.name))
+
+            let descriptionEl = document.createElement('p')
+            descriptionEl.appendChild(document.createTextNode(repo.description))
+
+            let linkEl = document.createElement('a')
+            linkEl.setAttribute('target', '_blank')
+            linkEl.setAttribute('href', repo.html_url)
+            linkEl.appendChild(document.createTextNode('Acessar'))
+
+            let listItemEl = document.createElement('li')
+            listItemEl.appendChild(imgEl)
+            listItemEl.appendChild(titleEl)
+            listItemEl.appendChild(descriptionEl)
+            listItemEl.appendChild(linkEl)
+
+
+            this.listEl.appendChild(listItemEl)
+
+        })
+
+
+
+
+    }
+
+
 }
 
-Api.getUserInfo('marlloncampos')
-Api.getUserInfo('asdasd23123131asdass')
+new App();
